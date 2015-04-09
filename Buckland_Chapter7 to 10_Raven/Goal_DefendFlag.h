e414a5b22b0361253484fd0219fe5391abda84f7
azero18 <absolutezero18@gmail.com>
2015-04-09 19:11:15 -0300

    defend flag.

---
+++ Buckland_Chapter7 to 10_Raven/Goal_DefendFlag.h
@@ -0,0 +1,54 @@
+#ifndef GOAL_DEF_FLAG_H
+#define GOAL_DEF_FLAG_H
+#pragma warning (disable:4786)
+
+#include "Goals/Goal_Composite.h"
+#include "goals/Raven_Goal_Types.h"
+#include "Raven_Bot.h"
+#include "triggers/trigger.h"
+
+
+//helper function to change an item type enumeration into a goal type
+//int ItemTypeToGoalType(int gt);
+
+
+class Goal_DefendFlag : public Goal_Composite<Raven_Bot>
+{
+private:
+
+//  int                     m_iItemToGet;
+
+  Trigger<Raven_Bot>*     m_pGiverTrigger;
+
+  //true if a path to the item has been formulated
+  bool                    m_bFollowingPath;
+
+  //returns true if the bot sees that the item it is heading for has been
+  //picked up by an opponent
+  bool hasItemBeenStolen()const;
+
+public:
+
+  Goal_DefendFlag(Raven_Bot* pBot):Goal_Composite<Raven_Bot>(pBot,
+                                                   goal_defend_flag),
+                                //m_iItemToGet(item),
+                                m_pGiverTrigger(0),
+                                m_bFollowingPath(false)
+  {}
+
+
+  void Activate();
+
+  int  Process();
+
+  bool HandleMessage(const Telegram& msg);
+
+  void Terminate(){m_iStatus = completed;}
+};
+
+
+
+
+
+
+#endif
--- Buckland_Chapter7 to 10_Raven/goals/Raven_Goal_Types.h
+++ Buckland_Chapter7 to 10_Raven/goals/Raven_Goal_Types.h
@@ -24,7 +24,8 @@ enum
   goal_strafe,
   goal_adjust_range,
   goal_say_phrase,
-  goal_get_flag
+  goal_get_flag,
+  goal_defend_flag
   
 };
 
